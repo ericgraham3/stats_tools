67a0065cbb727b6081b442d5176a68dcbae500e4
ericgraham3 <eric.graham@gmail.com>
2024-01-30 20:20:33 -0600

    created class and methods for frequency table

--- frequency.py
+++ frequency.py
@@ -4,18 +4,21 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
-# reading csv file as pandas dataframe
-data = pd.read_csv('table.csv')
 
-# create frequency table
-freq_table = pd.crosstab(data.iloc[:, 0], 'frequency')
+class Frequency:
+    def __init__(self, user_csv):
+        self.user_csv = user_csv
+        self.df = pd.read_csv(self.user_csv)
 
-# count data points in dataset and take square root of that value to determine number of bins in histogram
-n_data = len(data)
-n_bins = np.sqrt(n_data)
-n_bins = n_bins.astype(int)
+    def frequency_table(self):
+        freq_table = pd.crosstab(self.df.iloc[:, 0], 'frequency')
 
-# create and show histogram and frequency table
-plt.hist(data, bins=n_bins)
-plt.show()
-print(freq_table)
+        # count data points in dataset and take square root of that value to determine number of bins in histogram
+        n_data = len(data)
+        n_bins = np.sqrt(n_data)
+        n_bins = n_bins.astype(int)
+
+        # create and show histogram and frequency table
+        plt.hist(self.df, bins=n_bins)
+        plt.show()
+        print(freq_table)
